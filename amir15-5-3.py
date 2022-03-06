13eedc0b032a7cd6e36a3ddb7e41f5f3478cbd69
adirm88 <adirm88@gmail.com>
2022-03-05 22:27:52 -0800

    added .idea to gitignore file

---
+++ amir15-5-3.py
@@ -0,0 +1,19 @@
+
+def amir_15_5_3():
+    a = []
+    for i in range(0, 100):
+        if i == 0: continue
+        if i % 15 == 0:
+            print(i)
+            continue
+        if i % 3 == 0:
+            a.append(i)
+        if i % 5 == 0:
+            a.append(i)
+    for x in a:
+        print(x)
+
+
+
+if __name__ == '__main__':
+    amir_15_5_3()
